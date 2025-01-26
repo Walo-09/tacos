690ee379e5e9f6a1e5bf222d71fd4c17a33ddee1
Walo-09 <ooswaldosalas0901@gmail.com>
2025-01-26 00:54:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("yes").addEventListener("click", function() {
+    alert("¡Sabía que dirías que sí! 💖");
+    document.getElementById("sticker").style.display = "block";
+});
+
+document.getElementById("no").addEventListener("mouseover", function() {
+    let button = document.getElementById("no");
+    let randomX = Math.random() * window.innerWidth - 100;
+    let randomY = Math.random() * window.innerHeight - 50;
+    button.style.position = "absolute";
+    button.style.left = `${randomX}px`;
+    button.style.top = `${randomY}px`;
+});
